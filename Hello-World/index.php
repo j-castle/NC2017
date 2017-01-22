1a0693b41195a8f97f8b98773c1a46d72f272948
j-castle <j-castle.1@onu.edu>
2017-01-22 15:16:30 -0500

    Add files via upload

---
+++ Hello-World/index.php
@@ -0,0 +1,12 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Hello World!</title>
+    </head>
+    <body>
+        <?php
+        Print "Hello World!";
+        ?>
+    </body>
+</html>
