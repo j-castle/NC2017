06f7b14a1383878775558aecf69d7e0d24b9afde
j-castle <j-castle.1@onu.edu>
2017-01-26 16:37:28 -0500

    Add files via upload

---
+++ Hello-World Revamp/index.php
@@ -0,0 +1,21 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="main.css">
+        <title>Hello World!</title>
+    </head>
+    <body>
+        <p>
+            Where am I?
+        </p>
+        
+        <p>
+            Hello, World!
+        </p>
+        
+        <div>
+            Hello, Mr. <span class="blue">Blue</span>
+        </div>
+    </body>
+</html>
