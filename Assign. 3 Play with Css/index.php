4f654fb36bc7cfab17521f106eeead5df011f971
j-castle <j-castle.1@onu.edu>
2017-01-31 17:10:56 -0500

    Add files via upload

---
+++ Assign. 3 Play with Css/index.php
@@ -0,0 +1,35 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="main.css">
+        <title>Star Wars</title>
+    </head>
+    <body>
+        <div class="hello1">
+            Hello, Star <span class="vader">Wars</span>
+        </div> 
+
+        <div class="pic">
+        <p class="luke">
+            #2
+        </p>
+        
+        <p class="han">
+            #1
+        </p>
+        
+        <p class="star">
+            #3
+        </p>
+        
+        <p class="darth">
+            #4
+        </p>
+        </div>
+        
+        <div class="hello">
+            Hello, Star <span class="vader">Wars</span>
+        </div>
+    </body>
+</html>
