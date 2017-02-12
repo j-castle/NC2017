6a93950833dc3bcc55c53a45b6721614cb7acdfa
j-castle <j-castle.1@onu.edu>
2017-02-12 04:01:40 -0500

    Add files via upload

---
+++ Assign. 4 Mimic a Printed Ad/index.php
@@ -0,0 +1,76 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="main.css">
+        <title>Nintendo Ad</title>
+    </head>
+    <body>       
+        <div class="power">
+            <span class='child'>
+                EVERYTHING ELSE IS CHILD'S PLAY.
+            </span>
+        </div>
+        
+        <p>
+            
+            <span class='indent'>
+                  The Power Glove<sup class='super'>TM</sup>. You plug it in like any joystick, But the similarity stops there.
+            </span>
+            
+            <span class='left'>
+                Because now you don't just guide the action. You are the action.
+            </span>
+            
+            <span class='indent'>
+                3-D sensors track the position of your hand, giving you free-flowing, instant 
+            </span>
+            
+            <span class='left'>
+                response. It's a complete connection. Intense. And powerful.
+            </span>
+            
+            <span class="indent">
+                Plus, the Power Glove has a unique programmable keypad that gives you amazing
+            </span>
+            
+            <span class='left'>
+                new ways to play almost every Nintendo<sup class='super'>©</sup> game, All your joystick games become
+            </span>
+            
+            <span class='left'>
+                different. More exciting. And with games specifically designed for the Power Glove,
+            </span>
+            
+            <span class='left'>
+                you'll be blown into another dimension.
+            </span>
+            
+            <span class='indent'>
+                So look for the Power Glove when it hits stores this Fall. Once you put it on, 
+            </span>
+            
+            <span class='left'>
+               everything else becomes child's play. 
+            </span>
+            
+            <img class='nes' src="nes.png" alt="nes" style="width:87px">
+            <img class='seal' src="seal.png" alt="seal" style="width:52px">
+            <img class='mattel' src="mattel.jpg" alt="mattel" style="width:23px">
+            
+            <span class='glove'>
+                POWER GLOVE<sup class='super'>TM</sup>
+            </span>
+            
+            <span class='print'>
+                © Mattel Inc. 1998 All Rights Reserved. Nintendo Entertainment System are registered trademarks of Nintendo Inc. used 
+            </span>
+            
+            <span class='print'>
+                under license. Manufactured under license from Abrame/Gentile Entertainment, Inc. Lightning bolts not included.
+            </span>
+            
+        </p>
+
+    </body>
+</html>
