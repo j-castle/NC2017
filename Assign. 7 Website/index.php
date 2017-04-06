89eae8976ca96924b0702d2b9e486387534f5e20
j-castle <j-castle.1@onu.edu>
2017-04-05 20:42:22 -0400

    Add files via upload

---
+++ Assign. 7 Website/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="WebSite.css">
+        <title>Net-Centric Assignments</title>
+        <h1>Net-Centric Assignments</h1>
+        <h2>Jaden Castle</h2>
+    </head>
+    <body>
+        <p>
+            <ul id="list" style="list-style-type:disc">
+                <li><a href="http://nccastle.onucs.org/JavaScript_Play/JavaScript_Play.php" target="_blank">Javascript Play</a></li> 
+                <li><a href="http://nccastle.onucs.org//Calendar/Cal.php" target="_blank">PHP 1</a></li> 
+                <li><a href="http://nccastle.onucs.org/nintendoAd/index.php" target="_blank">Mimic a Print Advertisement</a></li> 
+                <li><a href="http://nccastle.onucs.org/PlayWithCSS/index.php" target="_blank">Play with CSS</a></li> 
+                <li><a href="http://nccastle.onucs.org/HelloWorldRevamp/index.php" target="_blank">Innovative Hello World</a> 
+                <li><a href="http://nccastle.onucs.org/HelloWorld/index.php" target="_blank">Hello World</a></li> 
+            </ul> 
+        </p>
+        
+        <p id="video">
+            <iframe width="420" height="315"
+            src="https://www.youtube.com/embed/bWPMSSsVdPk">
+            </iframe>
+            <iframe width="420" height="315"
+            src="https://www.youtube.com/embed/ZdP0KM49IVk">
+            </iframe>
+            <iframe width="420" height="315"
+            src="https://www.youtube.com/embed/0afZj1G0BIE">
+            </iframe>
+        </p>
+    </body>
+</html>
